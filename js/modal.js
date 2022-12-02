e752cb781805b394c97f9e521814050731088112
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2022-12-02 18:39:06 +0200

    Refactored code
    Refactored js

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,35 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector(".backdrop"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.backdrop.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+// const openModalButtons = document.querySelectorAll('.data-modal-open'),
+//       modal = document.querySelector('.data-modal'),
+//   closeModalButtons = document.querySelectorAll('.data-modal-close'),
+//   closeModalAll = document.querySelectorAll('.backdrop');
+      
+//   openModalButtons.forEach(openModalButtons => {
+//   openModalButtons.addEventListener('click', toggleModal)
+// });
+
+//   closeModalButtons.forEach(closeModalButtons => {
+//   closeModalButtons.addEventListener('click', toggleModal)
+//   });
+
+//   closeModalAll.forEach(closeModalAll => {
+//   closeModalAll.addEventListener('click', toggleModal)
+// });
+
+// function toggleModal() {
+//     modal.classList.toggle("is-hidden");
+//   }
--- js/scroll-to-top.js
+++ js/scroll-to-top.js
@@ -1,7 +1,7 @@
 const scrollBtn = document.querySelector(".scroll-wrapper__btn");
 
 const btnVisibility = () => {
-    if (window.scrollY > 600) {
+    if (window.scrollY > 400) {
         scrollBtn.style.visibility = "visible";
     } else {
         scrollBtn.style.visibility = "hidden";
