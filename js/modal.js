f7e7762daf21f8cf96f456d20f0dc5d000c3ca4d
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2022-12-03 21:21:46 +0200

    Refactored code
    Update scroll-button

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,12 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    backdrop: document.querySelector(".backdrop"),
+    backdrop: document.getElementsByClassName(".data-modal"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-  refs.backdrop.addEventListener("click", toggleModal);
+  // refs.backdrop.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
